6ec406495d64eeb428704dcede05edc0065acfae
Jordan Stammer <s.jordanstammer@gmail.com>
2024-03-02 16:18:18 -0500

    Render Tasks built
    Selecting a project now renders associated task names

--- src/index.js
+++ src/index.js
@@ -3,28 +3,57 @@ import data from './data.js'
 
 
 let DOMController = (function(){
+    const nav = document.querySelector('.sidebar')
+    const tasklist = document.querySelector('.tasks')
+    
+
     document.querySelector('.save').addEventListener('click', () => data.save())
     document.querySelector('.test').addEventListener('click', () => { 
-        render()
+        renderTasks()
     })
 
-    const nav = document.querySelector('.sidebar')
-
+    
     function render(){  
-        let projectIndex = 0;
+        renderProjects()
+}
+    function renderProjects(){
+        let i = 0;
         nav.innerHTML = ``
         data.get().forEach(project => {
         const li = document.createElement('li');
-        li.setAttribute('data-index', projectIndex)
-        projectIndex ++;
+        li.setAttribute('data-index', i)
+        i ++;
         li.textContent = project.name;
         li.addEventListener('click', () =>{
         data.selectProject(li.getAttribute('data-index'))
-        console.log('Logging Current', data.getSelected())
+        // renderTasks();
         })
         nav.appendChild(li)
 
     });
-}
+    }
+
+    function renderTasks(){
+        tasklist.innerHTML = ``
+        const tasks = data.get()[data.getSelected()].tasks
+        let i = 0
+        tasks.forEach(task => {
+            const div = document.createElement('div');
+            div.className = 'task'
+            div.setAttribute('data-index', i)
+            let title = document.createElement('h2')
+            title.className = 'task-title';
+            title.textContent = task.title
+            div.appendChild(title)
+            tasklist.appendChild(div)
+            i++
+        });
+        
+
+
+    }
+
+    render()
     return {render}
 })()
+
